d98c6b78607bd473f36f729bd77084cd9e7bb96b
mars.huang <mars.huang@d1m.cn>
2020-07-20 16:00:44 +0800

    [build] 打包增加进度条

--- build/build.lib.js
+++ build/build.lib.js
@@ -1,6 +1,8 @@
 const { sh } = require('tasksfile')
 const fs = require('fs')
 const rimraf = require('rimraf')
+const chalk = require('chalk')
+const ProgressBar = require('progress')
 const { getAssetsPath, fsExistsSync, moveFile } = require('./utils')
 const libList = require('../src/lib-list')
 const { outputPath, styleOutputPath } = require('./config')
@@ -11,6 +13,8 @@ const build = function (data = {}, index, arr) {
   const { input, output } = data
 
   sh(`vue-cli-service build --target lib --no-clean --name ${output} --dest ${getAssetsPath(outputPath)} ${input}`)
+
+  bar.tick()
 
   if (fsExistsSync(getAssetsPath(outputPath, `${output}.css`))) {
     cssFiles.push(`${output}.css`)
@@ -23,7 +27,15 @@ Object.keys(libList).forEach((name) => {
   }
 })
 
+const bar = new ProgressBar('[:bar] :percent ', { total: pkgArray.length })
+
 pkgArray.forEach(build)
+
+if (bar.complete) {
+  console.log(chalk.green('========================================='))
+  console.log(chalk.green('========打包成功(build success)!========='))
+  console.log(chalk.green('========================================='))
+}
 
 rimraf(getAssetsPath(outputPath, 'demo.html'), (err) => {
   console.log(err)
